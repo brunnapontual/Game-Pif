9c283b005441fdda511f9fe7d0666ed8972c1e7b
brunnapontual <brunnapontual@hotmail.com>
2024-11-08 23:42:58 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,10 +4,11 @@
  * Author: Tiago Barros
  * Based on "From C to C++ course - 2002"
 */
-
 #include "keyboard.h"
 #include "mylib.h"
 #include "screen.h"
+#include "timer.h"
+
 
 int main() {
   SnakeP *cobra = NULL;
@@ -15,9 +16,22 @@ int main() {
   FILE *arquivo;
   Jogador jogador;
 
+  printf("Digite o nome do jogador: ");
+  scanf("%s", jogador.nome);
   screenInit(1);
   keyboardInit();
+  timerInit(intervalo);
+
+  aumentarTamanho(&cobra, 34, 12);
+  diminuirTamanho(&cobra);
+  srand((unsigned int)time(NULL));
+  int frutaX = rand() % 68 + 8, frutaY = rand() % 18 + 2;
+  TipoFruta tipoFruta = rand() % 2 == 0 ? FRUTA_COLORIDA : FRUTA_PRETO;
+  desenharFruta(frutaX, frutaY, tipoFruta);
+  Fruta fruta = {frutaX, frutaY, tipoFruta};
+  verificarFrutaComida(&cobra, &fruta);
   screenUpdate();
+  sleep(3);
 
   while (tecla != SAIR) {
     if (keyhit()) {
@@ -49,8 +63,49 @@ int main() {
         break;
       }
       screenUpdate();
+    }
+    if (timerTimeOver)) {
+      int novoX = cobra->posX + dirX, novoY = cobra->posY + dirY;
+      if (novoX >= MAXX || novoX <= MINX || novoY >= MAXY || novoY <= MINY || verificarColisao(cobra, novoX, novoY)){
+        break;
       }
+      if (novoX == frutaX && novoY == frutaY) {
+        Fruta fruta = {frutaX, frutaY, tipoFruta};
+        verificarFrutaComida(&cobra, &fruta);
+        novaFruta(&frutaX, &frutaY, &tipoFruta);
+        desenharFruta(frutaX, frutaY, tipoFruta);
+        pontos++;
+        intervalo = (intervalo > 20) ? (intervalo - 5) : 20;
+        timerInit(intervalo);
+      }
+      atualizarPlacar(pontos);
+      limparCobra(cobra);
+      movimentoCobra(&cobra, novoX, novoY);
+      desenharCobra(cobra);
+      screenUpdate();
     }
   }
+  freeCobra(&cobra);
+  keyboardDestroy();
+  screenDestroy();
+  jogador.pontos = pontos;
+  arquivo = fopen("ranking.txt", "a");
+  fwrite(&jogador, sizeof(Jogador), 1, arquivo);
+  fclose(arquivo);
+
+  Placar *ranking = NULL;
+  arquivo = fopen("ranking.txt", "r");
+  while (fread(&jogador, sizeof(Jogador), 1, arquivo) == 1) {
+    adicionarPlacar(&ranking, jogador);
+  }
+  fclose(arquivo);
+  arquivo = fopen("ranking.txt", "w");
+  salvarPlacar(ranking, arquivo);
+  fclose(arquivo);
+  imprimirRanking(ranking);
+  GameOver();
+  freePlacar(&ranking);
+  timerDestroy();
+  
   return 0;
 }
